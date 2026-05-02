a54fe7657a5af3f9280ffb48fb5fd373e52ed3b6
oyoyo4556 <nao121123@gmail.com>
2026-05-02 19:40:31 +0900

    update ver.1.3.0
    Fixed how masks are handled when “done”. Made various related changes. Also relaxed the compression for qnet from 512 to 53.

---
+++ common.rs
@@ -0,0 +1,12 @@
+use crate::env::RawState;
+pub struct Experience {
+    pub state:RawState,
+    pub action:u8,
+    pub reward:f32,
+    pub next_state:RawState,
+    pub done:bool,
+    pub next_gamma: f32,
+}
+
+pub const TRAIN_AGENT_ID:usize = 0;
+pub const INPUT_STATE_DIM:usize = 233;
